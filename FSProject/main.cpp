85f9bc44f68917054b07f0edbf3d87fac53a7e08
A1 <ahamayama13@gmail.com>
2024-10-04 11:31:41 +0900

    マクロ削除

--- FSProject/main.cpp
+++ FSProject/main.cpp
@@ -2,16 +2,6 @@
 #include <iostream>
 #include <iostream>
 
-void Print(List<int> &list) {
-	std::cout << "list : ";
-
-	for (auto &i : list) {
-		std::cout << i << ", ";
-	}
-
-	std::cout << std::endl;
-}
-
 int main(void) {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	
