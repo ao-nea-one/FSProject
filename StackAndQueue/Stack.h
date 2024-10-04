fff8615f3e8d10cd9d45d0dd06374a71ff0f9c9a
A1 <ahamayama13@gmail.com>
2024-10-04 16:21:07 +0900

    Stack作成開始

---
+++ StackAndQueue/Stack.h
@@ -0,0 +1,42 @@
+/*--------------------------------------------------
+	Name : Stack
+	Date : 2024/10/01
+--------------------------------------------------*/
+
+#pragma once
+#ifndef _STACK_H_
+#define _STACK_H_
+
+/*--- include ---*/
+#include "../FSProject/List.h"
+
+
+
+/// <summary>
+/// Stackクラス
+/// </summary>
+/// <typeparam name="T"></typeparam>
+template<class T>
+class Stack {
+private:
+	/*--- メンバー変数 ---*/
+
+	List<T> list;
+
+
+
+public:
+	/*--- メンバー関数 ---*/
+
+
+
+public:
+	/*--- アクセサ関数 ---*/
+
+	unsigned int GetCount(void) const {
+		//list
+	}
+};
+
+
+#endif
