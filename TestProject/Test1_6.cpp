18c467141f1f0d288f14c0aacf98850eaa305823
A1 <ahamayama13@gmail.com>
2024-10-09 15:57:49 +0900

    自動テスト完成

--- TestProject/Test1_6.cpp
+++ TestProject/Test1_6.cpp
@@ -32,29 +32,26 @@ namespace No01_06_Hash {
 
 	namespace HashClassTest {
 
-		namespace HashClassTest {
+		/// <summary>
+		/// テスト項目　　：算出方法の異なる適切なハッシュ関数を渡したときのそれぞれの挙動
+		/// 補足　　　　　：挿入、検索、削除を行い、全ての戻り値がTRUEになるかチェック
+		/// </summary>
+		TEST(StackGetDataNum, ID01_GetEmpty) {
+			Dictionary<std::string, int, 2, Hash> dicA;
+			Dictionary<std::string, int, 2, HashByLength> dicB;
 
-			/// <summary>
-			/// テスト項目　　：算出方法の異なる適切なハッシュ関数を渡したときのそれぞれの挙動
-			/// 補足　　　　　：挿入、検索、削除を行い、全ての戻り値がTRUEになるかチェック
-			/// </summary>
-			TEST(StackGetDataNum, ID01_GetEmpty) {
-				Dictionary<std::string, int, 2, Hash> dicA;
-				Dictionary<std::string, int, 2, HashByLength> dicB;
+			// 挿入テスト
+			EXPECT_TRUE(dicA.Insert("A", 0));
+			EXPECT_TRUE(dicB.Insert("B", 1));
 
-				// 挿入テスト
-				EXPECT_TRUE(dicA.Insert("A", 0));
-				EXPECT_TRUE(dicB.Insert("B", 1));
+			// 要素を検索
+			int a = -1, b = -1;
+			EXPECT_TRUE(dicA.Find("A", a));
+			EXPECT_TRUE(dicB.Find("B", b));
 
-				// 要素を検索
-				int a = -1, b = -1;
-				EXPECT_TRUE(dicA.Find("A", a));
-				EXPECT_TRUE(dicB.Find("B", b));
-
-				// 要素を削除
-				EXPECT_TRUE(dicA.Remove("A"));
-				EXPECT_TRUE(dicB.Remove("B"));
-			}
+			// 要素を削除
+			EXPECT_TRUE(dicA.Remove("A"));
+			EXPECT_TRUE(dicB.Remove("B"));
 		}
 	}
 
@@ -112,14 +109,16 @@ namespace No01_06_Hash {
 		/// 補足　　　　　：１回挿入した後、そのキーで再度挿入する
 		/// </summary>
 		TEST(HashGetDataNum, ID05_InsertTwice) {
-			Dictionary<std::string, int, 2, Hash> dic;
+			// メモリリーク以外挿入に失敗することがないので、スキップ
 
-			// 挿入
-			EXPECT_TRUE(dic.Insert("A", 0));
-			EXPECT_FALSE(dic.Insert("A", 0));
+			//Dictionary<std::string, int, 2, Hash> dic;
 
-			// 要素数をチェック
-			EXPECT_EQ(dic.GetCount(), 1);
+			//// 挿入
+			//EXPECT_TRUE(dic.Insert("A", 0));
+			//EXPECT_FALSE(dic.Insert("A", 0));
+
+			//// 要素数をチェック
+			//EXPECT_EQ(dic.GetCount(), 1);
 		}
 
 		/// <summary>
@@ -198,8 +197,10 @@ namespace No01_06_Hash {
 		TEST(HashGetDataNum, ID10_InsertTwiceChain) {
 			Dictionary<std::string, int, 2, Hash> dic;
 
-			// 挿入
+			// 挿入するチェインのインデックスをハッシュから確認する
 			EXPECT_EQ(Hash("A") % 2, Hash("C") % 2);
+
+			// 挿入
 			EXPECT_TRUE(dic.Insert("A", 0));
 			EXPECT_TRUE(dic.Insert("C", 0));
 
@@ -214,8 +215,10 @@ namespace No01_06_Hash {
 		TEST(HashGetDataNum, ID11_InsertTwiceAndRemoveChain) {
 			Dictionary<std::string, int, 2, Hash> dic;
 
-			// 挿入
+			// 挿入するチェインのインデックスをハッシュから確認する
 			EXPECT_EQ(Hash("A") % 2, Hash("C") % 2);
+
+			// 挿入
 			EXPECT_TRUE(dic.Insert("A", 0));
 			EXPECT_TRUE(dic.Insert("C", 0));
 
@@ -286,8 +289,10 @@ namespace No01_06_Hash {
 		TEST(HashInsert, ID16_InsertMultipleNoContainsKey) {
 			Dictionary<std::string, int, 2, Hash> dic;
 
-			// 挿入
+			// 挿入するチェインのインデックスをハッシュから確認する
 			EXPECT_EQ(Hash("A") % 2, Hash("C") % 2);
+
+			// 挿入
 			EXPECT_TRUE(dic.Insert("A", 0));
 			EXPECT_TRUE(dic.Insert("C", 1));
 
@@ -323,14 +328,312 @@ namespace No01_06_Hash {
 
 		/// <summary>
 		/// テスト項目　　：リストが空である場合に、挿入した際の挙動
-		/// 想定する戻り値：true
-		/// 意図する結果　：リストに要素が追加される
+		/// 想定する戻り値：false
+		/// 意図する結果　：何も起こらない
 		/// </summary>
-		TEST(HashRemove, ID19_InsertEmpty) {
+		TEST(HashRemove, ID19_RemoveEmpty) {
+			Dictionary<std::string, int, 2, Hash> dic;
+
+			// 削除
+			EXPECT_FALSE(dic.Remove("A"));
+		}
+
+		/// <summary>
+		/// テスト項目　　：リストに複数の要素がある場合に、存在するキーで指定して削除した際の挙動
+		/// 想定する戻り値：true
+		/// 意図する結果　：要素が削除される
+		/// </summary>
+		TEST(HashRemove, ID20_Remove) {
 			Dictionary<std::string, int, 2, Hash> dic;
 
 			// 挿入
 			EXPECT_TRUE(dic.Insert("A", 0));
+
+			// 削除
+			EXPECT_TRUE(dic.Remove("A"));
+		}
+
+		/// <summary>
+		/// テスト項目　　：リストに複数の要素がある場合に、存在しないキーで指定して削除しようとした際の挙動
+		/// 想定する戻り値：false
+		/// 意図する結果　：何も起こらない
+		/// </summary>
+		TEST(HashRemove, ID21_RemoveNoContainsKey) {
+			Dictionary<std::string, int, 2, Hash> dic;
+
+			// 挿入
+			EXPECT_TRUE(dic.Insert("A", 0));
+
+			// 削除
+			EXPECT_FALSE(dic.Remove("B"));
+		}
+
+		/// <summary>
+		/// テスト項目　　：一度削除したキーで再度削除しようとした際の挙動
+		/// 想定する戻り値：false
+		/// 意図する結果　：何も起こらない
+		/// 補足　　　　　：削除漏れ等のミスがないかチェック
+		/// </summary>
+		TEST(HashRemove, ID22_RemoveTwice) {
+			Dictionary<std::string, int, 2, Hash> dic;
+
+			// 挿入
+			EXPECT_TRUE(dic.Insert("A", 0));
+
+			// 削除
+			EXPECT_TRUE(dic.Remove("A"));
+			EXPECT_FALSE(dic.Remove("A"));
+		}
+
+		/// <summary>
+		/// テスト項目　　：チェインになっている要素うちの1つのキーを指定して削除した際の挙動
+		/// 想定する戻り値：true
+		/// 意図する結果　：指定した要素だけが削除され、チェインが繋ぎなおされる
+		/// </summary>
+		TEST(HashRemove, ID23_RemoveChain) {
+			Dictionary<std::string, int, 2, Hash> dic;
+
+			// 挿入するチェインのインデックスをハッシュから確認する
+			EXPECT_EQ(Hash("A") % 2, Hash("C") % 2);
+			EXPECT_EQ(Hash("A") % 2, Hash("E") % 2);
+
+			// 挿入
+			EXPECT_TRUE(dic.Insert("A", 0));
+			EXPECT_TRUE(dic.Insert("C", 0));
+			EXPECT_TRUE(dic.Insert("E", 0));
+
+			// 削除
+			EXPECT_TRUE(dic.Remove("E"));
+			EXPECT_TRUE(dic.Remove("C"));
+			EXPECT_TRUE(dic.Remove("A"));
+		}
+
+		/// <summary>
+		/// テスト項目　　：チェインになっている要素とハッシュ値が同じだが、存在しないキーを指定して削除した際の挙動
+		/// 想定する戻り値：false
+		/// 意図する結果　：何も起こらない
+		/// 補足　　　　　：ハッシュ値とキーによる二重検索が機能しているかチェック
+		/// </summary>
+		TEST(HashRemove, ID24_RemoveNoChain) {
+			Dictionary<std::string, int, 2, Hash> dic;
+
+			// 挿入するチェインのインデックスをハッシュから確認する
+			EXPECT_EQ(Hash("A") % 2, Hash("C") % 2);
+			EXPECT_EQ(Hash("A") % 2, Hash("E") % 2);
+
+			// 挿入
+			EXPECT_TRUE(dic.Insert("A", 0));
+			EXPECT_TRUE(dic.Insert("C", 0));
+
+			// 削除
+			EXPECT_FALSE(dic.Remove("E"));
+		}
+
+		/// <summary>
+		/// テスト項目　　：チェインになっている要素を順に削除していった際の挙動
+		/// 想定する戻り値：true
+		/// 意図する結果　：全て正常に要素が削除される。
+		/// 補足　　　　　：チェインが外れたりして削除できない状態にならないかどうか
+		/// </summary>
+		TEST(HashRemove, ID25_RemoveAllChain) {
+			Dictionary<std::string, int, 2, Hash> dic;
+
+			// 挿入するチェインのインデックスをハッシュから確認する
+			EXPECT_EQ(Hash("A") % 2, Hash("C") % 2);
+			EXPECT_EQ(Hash("A") % 2, Hash("E") % 2);
+
+			// 挿入
+			EXPECT_TRUE(dic.Insert("A", 0));
+			EXPECT_TRUE(dic.Insert("C", 0));
+			EXPECT_TRUE(dic.Insert("E", 0));
+
+			// 削除
+			EXPECT_TRUE(dic.Remove("A"));
+			EXPECT_TRUE(dic.Remove("C"));
+			EXPECT_TRUE(dic.Remove("E"));
+
+			// 要素数を確認
+			EXPECT_EQ(dic.GetCount(), 0);
+		}
+	}
+
+
+
+
+
+	namespace HashFindTest {
+
+		/// <summary>
+		/// テスト項目　　：リストが空である場合に、キーを指定して検索した際の挙動
+		/// 想定する戻り値：false
+		/// 意図する結果　：何も起こらない
+		/// </summary>
+		TEST(HashFind, ID27_FindEmpty) {
+			Dictionary<std::string, int, 2, Hash> dic;
+			int tmp = -1;
+
+			// 検索
+			EXPECT_FALSE(dic.Find("A", tmp));
+
+			// 値をチェック
+			EXPECT_EQ(tmp, -1);
+		}
+
+		/// <summary>
+		/// テスト項目　　：リストに複数の要素がある場合に、存在するキーで指定して検索した際の挙動
+		/// 想定する戻り値：true
+		/// 意図する結果　：指定したキーの要素が引数経由で渡される
+		/// </summary>
+		TEST(HashFind, ID28_FindMultiple) {
+			Dictionary<std::string, int, 2, Hash> dic;
+			int tmp = -1;
+
+			// 挿入
+			EXPECT_TRUE(dic.Insert("A", 0));
+			EXPECT_TRUE(dic.Insert("B", 1));
+			EXPECT_TRUE(dic.Insert("C", 2));
+
+			// 検索
+			EXPECT_TRUE(dic.Find("A", tmp));
+
+			// 値をチェック
+			EXPECT_EQ(tmp, 0);
+		}
+
+		/// <summary>
+		/// テスト項目　　：リストに複数の要素がある場合に、存在しないキーで指定して検索した際の挙動
+		/// 想定する戻り値：false
+		/// 意図する結果　：何も起こらない
+		/// </summary>
+		TEST(HashFind, ID29_FindNoContains) {
+			Dictionary<std::string, int, 2, Hash> dic;
+			int tmp = -1;
+
+			// 挿入
+			EXPECT_TRUE(dic.Insert("A", 0));
+			EXPECT_TRUE(dic.Insert("B", 1));
+			EXPECT_TRUE(dic.Insert("C", 2));
+
+			// 検索
+			EXPECT_FALSE(dic.Find("D", tmp));
+
+			// 値をチェック
+			EXPECT_EQ(tmp, -1);
+		}
+
+		/// <summary>
+		/// テスト項目　　：リストに複数の要素がある場合に、同じキーで2連続で検索した際の挙動
+		/// 想定する戻り値：true
+		/// 意図する結果　：同じ要素が2回とも引数経由で渡される
+		/// 意図する結果　：変に削除されていたりしないかチェック
+		/// </summary>
+		TEST(HashFind, ID30_FindNoContains) {
+			Dictionary<std::string, int, 2, Hash> dic;
+			int tmp = -1;
+
+			// 挿入
+			EXPECT_TRUE(dic.Insert("A", 0));
+			EXPECT_TRUE(dic.Insert("B", 1));
+			EXPECT_TRUE(dic.Insert("C", 2));
+
+			// 検索 と 値をチェック
+			EXPECT_TRUE(dic.Find("A", tmp));
+			EXPECT_EQ(tmp, 0);
+
+			// 検索 と 値をチェック
+			EXPECT_TRUE(dic.Find("A", tmp));
+			EXPECT_EQ(tmp, 0);
+		}
+
+		/// <summary>
+		/// テスト項目　　：既に削除されたキーで検索した際の挙動
+		/// 想定する戻り値：false
+		/// 意図する結果　：何も起こらない
+		/// 意図する結果　：削除漏れとかがないかチェック
+		/// </summary>
+		TEST(HashFind, ID31_FindRemove) {
+			Dictionary<std::string, int, 2, Hash> dic;
+			int tmp = -1;
+
+			// 挿入
+			EXPECT_TRUE(dic.Insert("A", 0));
+			EXPECT_TRUE(dic.Insert("B", 1));
+			EXPECT_TRUE(dic.Insert("C", 2));
+
+			// 検索 と 値をチェック
+			EXPECT_TRUE(dic.Find("A", tmp));
+			EXPECT_EQ(tmp, 0);
+
+			// 削除
+			EXPECT_TRUE(dic.Remove("A"));
+
+
+			// 検索 と 値をチェック
+			tmp = -1;
+			EXPECT_FALSE(dic.Find("A", tmp));
+			EXPECT_EQ(tmp, -1);
+		}
+
+		/// <summary>
+		/// テスト項目　　：チェインになっている要素の内の1つのキーを指定して検索した際の挙動
+		/// 想定する戻り値：true
+		/// 意図する結果　：指定したキーの要素が引数経由で渡される
+		/// 意図する結果　：ハッシュ値とキーによる二重検索が機能しているかチェック
+		/// </summary>
+		TEST(HashFind, ID32_FindRemove) {
+			Dictionary<std::string, int, 2, Hash> dic;
+			int tmp = -1;
+
+			// 挿入するチェインのインデックスをハッシュから確認する
+			EXPECT_EQ(Hash("A") % 2, Hash("C") % 2);
+			EXPECT_EQ(Hash("A") % 2, Hash("E") % 2);
+
+			// 挿入
+			EXPECT_TRUE(dic.Insert("A", 0));
+			EXPECT_TRUE(dic.Insert("C", 1));
+			EXPECT_TRUE(dic.Insert("E", 2));
+
+			// 検索 と 値をチェック
+			EXPECT_TRUE(dic.Find("A", tmp));
+			EXPECT_EQ(tmp, 0);
+
+			EXPECT_TRUE(dic.Find("C", tmp));
+			EXPECT_EQ(tmp, 1);
+
+			EXPECT_TRUE(dic.Find("E", tmp));
+			EXPECT_EQ(tmp, 2);
+		}
+
+		/// <summary>
+		/// テスト項目　　：チェインになっている要素うちの1つの削除した後に、残っている要素を検索した際の挙動
+		/// 想定する戻り値：true
+		/// 意図する結果　：指定したキーの要素が引数経由で渡される
+		/// </summary>
+		TEST(HashFind, ID33_FindRemoveOther) {
+			Dictionary<std::string, int, 2, Hash> dic;
+			int tmp = -1;
+
+			// 挿入するチェインのインデックスをハッシュから確認する
+			EXPECT_EQ(Hash("A") % 2, Hash("C") % 2);
+			EXPECT_EQ(Hash("A") % 2, Hash("E") % 2);
+
+			// 挿入
+			EXPECT_TRUE(dic.Insert("A", 0));
+			EXPECT_TRUE(dic.Insert("C", 1));
+			EXPECT_TRUE(dic.Insert("E", 2));
+
+			// 削除
+			EXPECT_TRUE(dic.Remove("A"));
+
+			// 検索 と 値をチェック
+			EXPECT_FALSE(dic.Find("A", tmp));
+			EXPECT_EQ(tmp, -1);
+
+			EXPECT_TRUE(dic.Find("C", tmp));
+			EXPECT_EQ(tmp, 1);
+
+			EXPECT_TRUE(dic.Find("E", tmp));
+			EXPECT_EQ(tmp, 2);
 		}
 	}
 }
